aae9dda302ded8ed6f3ab6d4e2463a784b50672e
iokoro222 <io2222@nyu.edu>
2025-11-03 13:38:23 -0500

    Update shiftcypher.py

--- shiftcypher.py
+++ shiftcypher.py
@@ -1,11 +1,23 @@
 import string
 
 in_str = input("Please enter your input: ")
-string = str(in_str)            
-    
 
-def e_func(string, e_key)
+string = str(in_str)            
+letters = list(string.ascii_lowercase)  
+
+key = {
+num_to_letter = { i: letters[i] for i in range(len(letters)) }  
+letter_to_num = { letters[i]: i for i in range(len(letters)) }
+
+
+    "num_to_letter": num_to_letter,
+    "letter_to_num": letter_to_num
+}    
+
+def e_func(string, key)
+output = key
+
 returns output
 
-def d_func(output, e_key)
+def d_func(output, key)
 return n_output
