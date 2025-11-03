77f833182f5b9a5112ea501723634d20f0cc2566
iokoro222 <io2222@nyu.edu>
2025-11-03 11:58:18 -0500

    Create shiftcypher.py

---
+++ shiftcypher.py
@@ -0,0 +1,11 @@
+import string
+
+in_str = input("Please enter your input: ")
+string = str(in_str)            
+    
+
+def e_func(string, e_key)
+returns output
+
+def d_func(output, e_key)
+return n_output
